ad56f44014374559cf4865d7202249120b2b9b46
nikita-adwani <niknikkynikita@gmail.com>
2019-06-18 10:52:33 +0530

    updated

--- angular/ssism/src/app/list/list.component.ts
+++ angular/ssism/src/app/list/list.component.ts
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-=======
 import { Component, OnInit, ɵCompiler_compileModuleSync__POST_R3__ } from '@angular/core';
 import { DataService } from '../services/data.service';
->>>>>>> 57d2cc9fe1112129dfca1f8037b86eafb9e8ad0a
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-<<<<<<< HEAD
-  styleUrls: ['./list.component.css']
-})
-export class ListComponent implements OnInit {
-canddidateName=['sonakshi','prashant','piyush','laveena','suhashi','nikita'];
-  constructor() { }
-
-  ngOnInit() {
-=======
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
@@ -33,7 +20,6 @@ export class ListComponent implements OnInit {
   ngOnInit() {
 
    
->>>>>>> 57d2cc9fe1112129dfca1f8037b86eafb9e8ad0a
   }
 
 }
