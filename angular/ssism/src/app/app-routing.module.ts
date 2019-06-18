ac561be6c4b2b4a027f47da6282d3e9d870330a5
Laveena-2806 <51774037+Laveena-2806@users.noreply.github.com>
2019-06-18 12:13:00 +0530

    more-detail component

--- angular/ssism/src/app/app-routing.module.ts
+++ angular/ssism/src/app/app-routing.module.ts
@@ -1,20 +1,14 @@
-import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { ListComponent } from './list/list.component';
-import { ProfileComponent } from './profile/profile.component';
+import { MoreDetailsComponent } from './more-details/more-details.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: ListComponent },
-  { path:'profile', component: ProfileComponent }
+  { path: 'list', component: ListComponent },
+  { path: 'more-details', component: MoreDetailsComponent },
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes),NgbModule],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {
+
+// export class AppRoutingModule{}
 
 
-
- }
