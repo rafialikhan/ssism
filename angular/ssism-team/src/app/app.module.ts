98ac7d3f44e17d05d28693e961e46a8133f3af2a
sona323 <ysonakshi57@gmail.com>
2019-06-29 23:17:48 +0530

    compeeted map task

--- angular/ssism-team/src/app/app.module.ts
+++ angular/ssism-team/src/app/app.module.ts
@@ -3,24 +3,18 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { DetailsComponent } from './details/details.component';
-=======
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { StudentsListComponent } from './students-list/students-list.component';
->>>>>>> b2ed5f0ab9bccb7a4ea7dfe3e1558ff3392dbf30
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    DetailsComponent
-=======
+    DetailsComponent,
     HeaderComponent,
     FooterComponent,
     StudentsListComponent
->>>>>>> b2ed5f0ab9bccb7a4ea7dfe3e1558ff3392dbf30
   ],
   imports: [
     BrowserModule,
