82ee8e761d9af322c839d9e5338a167f0297056b
pm9889 <51709274+pm9889@users.noreply.github.com>
2019-06-30 00:45:40 +0530

    my update

--- angular/ssism-team/src/app/app.module.ts
+++ angular/ssism-team/src/app/app.module.ts
@@ -3,24 +3,18 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { DetailsComponent } from './details/details.component';
-=======
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { StudentsListComponent } from './students-list/students-list.component';
->>>>>>> b2ed5f0ab9bccb7a4ea7dfe3e1558ff3392dbf30
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    DetailsComponent
-=======
     HeaderComponent,
     FooterComponent,
-    StudentsListComponent
->>>>>>> b2ed5f0ab9bccb7a4ea7dfe3e1558ff3392dbf30
+    StudentsListComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
