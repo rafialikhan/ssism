5a29260e4fa2962b594dedb4f94dbca69af45259
nikita-adwani <niknikkynikita@gmail.com>
2019-07-07 17:07:02 +0530

    reward.js

---
+++ team/reward.js
@@ -0,0 +1,400 @@
+var student = [{
+        studentName: 'Prashant Malviya',
+        studentID: 1,
+        courses: [{
+                courseName: 'HTML',
+                tasks: [{
+                        Name: 'HTML quiz',
+                        scores: 39,
+                        rewards: 100,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Designing a login form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Designing a registration form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    }
+                ]
+            },
+            {
+                courseName: 'Javascript',
+                tasks: [{
+                        Name: 'Javascript quiz',
+                        scores: 25,
+                        rewards: 100,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Validating a form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'filter,reduce,map ,json',
+                        scores: 10,
+                        reward: 100,
+                        Status: 'completed'
+                    },
+                    {
+                        Name: 'filter,map,reduce,and given json',
+                        scores: 10,
+                        reward: 100,
+                        Status: 'completed'
+                    },
+                    {
+                        Name: 'Webpage using dom',
+                        scores: 10,
+                        reward: 100,
+                        Status: 'completed'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        studentName: 'Piyush Sen',
+        studentID: 2,
+        courses: [{
+                courseName: 'HTML',
+                tasks: [{
+                        Name: 'HTML quiz',
+                        scores: 35,
+                        rewards: 100,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Designing a login form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Designing a registration form',
+                        scores: 5,
+                        rewards: 0,
+                        Status: 'complete'
+                    }
+                ]
+            },
+            {
+                courseName: 'Javascript',
+                tasks: [{
+                        Name: 'Javascript quiz',
+                        scores: 25,
+                        rewards: 100,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Validating a form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Validating a form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'filter,reduce,map ,json',
+                        scores: 10,
+                        reward: 100,
+                        Status: 'completed'
+                    },
+                    {
+                        Name: 'filter,map,reduce,and given json',
+                        scores: 10,
+                        reward: 100,
+                        Status: 'completed'
+                    },
+                    {
+                        Name: 'Webpage using dom',
+                        scores: 10,
+                        reward: 100,
+                        Status: 'completed'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        studentName: 'Nikita Adwani',
+        studentID: 3,
+        courses: [{
+                courseName: 'HTML',
+                tasks: [{
+                        Name: 'HTML quiz',
+                        scores: 40,
+                        rewards: 100,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Designing a login form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Designing a registration form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    }
+                ]
+            },
+            {
+                courseName: 'Javascript',
+                tasks: [{
+                        Name: 'Javascript quiz',
+                        scores: 25,
+                        rewards: 100,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Validating a form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Validating a form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'filter,reduce,map ,json',
+                        scores: 10,
+                        reward: 100,
+                        Status: 'completed'
+                    },
+                    {
+                        Name: 'filter,map,reduce,and given json',
+                        scores: 10,
+                        reward: 100,
+                        Status: 'completed'
+                    },
+                    {
+                        Name: 'Webpage using dom',
+                        scores: 10,
+                        reward: 100,
+                        Status: 'completed'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        studentName: 'Laveena pahuja',
+        studentID: 4,
+        courses: [{
+                courseName: 'HTML',
+                tasks: [{
+                        Name: 'HTML quiz',
+                        scores: 40,
+                        rewards: 100,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Designing a login form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Designing a registration form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    }
+                ]
+            },
+            {
+                courseName: 'Javascript',
+                tasks: [{
+                        Name: 'Javascript quiz',
+                        scores: 24,
+                        rewards: 100,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Validating a form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Validating a form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'filter,reduce,map ,json',
+                        scores: 10,
+                        reward: 100,
+                        Status: 'completed'
+                    },
+                    {
+                        Name: 'filter,map,reduce,and given json',
+                        scores: 10,
+                        reward: 100,
+                        Status: 'completed'
+                    },
+                    {
+                        Name: 'Webpage using dom',
+                        scores: 10,
+                        reward: 100,
+                        Status: 'completed'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        studentName: 'Sonakshi Yadav',
+        studentID: 5,
+        courses: [{
+                courseName: 'HTML',
+                tasks: [{
+                        Name: 'HTML quiz',
+                        scores: 35,
+                        rewards: 100,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Designing a login form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Designing a registration form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    }
+                ]
+            },
+            {
+                courseName: 'Javascript',
+                tasks: [{
+                        Name: 'Javascript quiz',
+                        scores: 25,
+                        rewards: 100,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Validating a form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    }
+                ]
+            },
+            {
+                Name: 'Validating a form',
+                scores: 10,
+                rewards: 0,
+                Status: 'complete'
+            },
+            {
+                Name: 'filter,reduce,map ,json',
+                scores: 10,
+                reward: 100,
+                Status: 'completed'
+            },
+            {
+                Name: 'filter,map,reduce,and given json',
+                scores: 10,
+                reward: 100,
+                Status: 'completed'
+            },
+            {
+                Name: 'Webpage using dom',
+                scores: 10,
+                reward: 100,
+                Status: 'completed'
+            }
+        ]
+    },
+    {
+        studentName: 'Suhasee Talreja',
+        studentID: 6,
+        courses: [{
+                courseName: 'HTML',
+                tasks: [{
+                        Name: 'HTML quiz',
+                        scores: 36,
+                        rewards: 100,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Designing a login form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Designing a registration form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    }
+                ]
+            },
+            {
+                courseName: 'Javascript',
+                tasks: [{
+
+
+
+                        Name: 'Javascript quiz',
+                        scores: 23,
+                        rewards: 100,
+                        Status: 'complete'
+                    },
+                    {
+                        Name: 'Validating a form',
+                        scores: 10,
+                        rewards: 0,
+                        Status: 'complete'
+                    }
+                ]
+            },
+            {
+                Name: 'Validating a form',
+                scores: 10,
+                rewards: 0,
+                Status: 'complete'
+            },
+            {
+                Name: 'filter,reduce,map ,json',
+                scores: 10,
+                reward: 100,
+                Status: 'completed'
+            },
+            {
+                Name: 'filter,map,reduce,and given json',
+                scores: 10,
+                reward: 100,
+                Status: 'completed'
+            },
+            {
+                Name: 'Webpage using dom',
+                scores: 10,
+                reward: 100,
+                Status: 'completed'
+            }
+        ]
+    }
+]
